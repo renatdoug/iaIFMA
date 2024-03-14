3dbd5110ccc7076378ca530dfcdcc4376268e7a1
RENATO DOUGLAS <83033433+renatdoug@users.noreply.github.com>
2024-03-14 08:14:34 -0300

    Update 01 prototype10.py

--- prototype10.py
+++ prototype10.py
@@ -5,13 +5,15 @@ from sklearn.preprocessing import LabelEncoder
 import csv
 import time
 
+def formatar_diagnostico(diagnostico):
+    return diagnostico.replace('_', ' ')
+
 def main():
     st.markdown("<h3 style='text-align: center; color: #FF5733;'>Sistema de Previsão de Diagnóstico de Enfermagem</h3>", unsafe_allow_html=True)
 
     # Carrega o DataFrame
     df = pd.read_csv('Dados.csv')
-    atributos = [coluna for coluna in df.columns if coluna !=
-                 'diagnostico_de_Enfermagem']
+    atributos = [coluna for coluna in df.columns if coluna != 'diagnostico_de_Enfermagem']
 
     # Carrega o modelo
     with open('modelo_4.pkl', 'rb') as file:
@@ -37,7 +39,7 @@ def main():
         label_encoder.fit(df['diagnostico_de_Enfermagem'])
 
         # Registro do tempo de início da seleção de sintomas
-        inicio_selecao = time.time()
+        inicio_secao = time.time()
 
         for sintoma in sintomas:
             dados_paciente_dict = {coluna: 0 for coluna in atributos}
@@ -49,8 +51,7 @@ def main():
 
             probas = best_tree.predict_proba([dados_paciente_np])[0]
 
-            indices_diagnosticos = [indice for indice, probabilidade in enumerate(
-                probas) if probabilidade > limiar_probabilidade]
+            indices_diagnosticos = [indice for indice, probabilidade in enumerate(probas) if probabilidade > limiar_probabilidade]
             for indice in indices_diagnosticos:
                 diagnostico = label_encoder.inverse_transform([indice])[0]
                 diagnosticos_associados.append((diagnostico, sintoma))
@@ -60,51 +61,61 @@ def main():
 
         for diagnostico, sintoma in diagnosticos_associados:
             sintoma_selecionado = sintoma.replace("_", " ")
-            sintoma_selecionado = sintoma_selecionado.replace(
-                "-", " ")  # Substituir traços por espaços
-            sintoma_selecionado = sintoma_selecionado.title()  # Capitalizar palavras
-            cuidado_key = f"Cuidado_{diagnostico}"
-            escolhido = st.checkbox(
-                f"Diagnóstico: {diagnostico.replace('_', ' ')}. Característica Definidora: {sintoma_selecionado}")
+            sintoma_selecionado = sintoma_selecionado.replace("-", " ")  # Substituir traços por espaços
+            sintoma_selecionado = sintoma_selecionado.lower()  # Capitalizar palavras
+            
+            # Verifica se o diagnóstico predito começa com "Risco"
+            if diagnostico.startswith("Risco"):
+                mensagem = f"Diagnóstico: {formatar_diagnostico(diagnostico)}. Fator Relacionado: {sintoma_selecionado}"
+            else:
+                mensagem = f"Diagnóstico: {formatar_diagnostico(diagnostico)}. Característica Definidora: {sintoma_selecionado}"
+                
+            escolhido = st.checkbox(mensagem)
+            
             if escolhido:
                 if diagnostico not in diagnosticos_selecionados:
                     diagnosticos_selecionados[diagnostico] = []
                 diagnosticos_selecionados[diagnostico].append(sintoma)
 
-    diagnostico_personalizado = st.text_area(
-        "Se desejar, escreva um diagnóstico personalizado:", "")
+
+    diagnostico_personalizado = st.text_area("Se desejar, escreva um diagnóstico personalizado:", "")
 
     if diagnostico_personalizado:
-        diagnosticos_selecionados["Personalizado"] = [
-            diagnostico_personalizado]
+        diagnosticos_selecionados["Personalizado"] = [diagnostico_personalizado]
 
     if diagnosticos_selecionados:
         st.subheader("Diagnósticos Selecionados:")
 
         for diagnostico, sintomas_selecionados in diagnosticos_selecionados.items():
-            sintomas_concatenados = ", ".join(sintomas_selecionados)
-            st.markdown(
-                f"<span style='font-size:18px'>{diagnostico.replace('_', ' ')}. Características definidoras: {sintomas_concatenados}</span>", unsafe_allow_html=True)
+            sintomas_formatados = [formatar_diagnostico(sintoma) for sintoma in sintomas_selecionados]
+
+            sintomas_concatenados = ", ".join(sintomas_formatados)
+
+            # Verifica se o diagnóstico selecionado começa com "Risco"
+            if diagnostico.startswith("Risco"):
+                mensagem = f"<span style='font-size:18px'>Diagnóstico: {formatar_diagnostico(diagnostico)}. Fatores Relacionados: {sintomas_concatenados}</span>"
+            else:
+                mensagem = f"<span style='font-size:18px'>Diagnóstico: {formatar_diagnostico(diagnostico)}. Características Definidoras: {sintomas_concatenados}</span>"
+
+            st.markdown(mensagem, unsafe_allow_html=True)
+
             if diagnostico in cuidados_df.columns:
                 cuidados_relacionados = cuidados_df[diagnostico].iloc[0]
                 if cuidados_relacionados:
                     st.write("")
-                    st.markdown(
-                        "&nbsp;&nbsp;&nbsp;&nbsp;**Cuidados Relacionados:**")
+                    st.markdown("&nbsp;&nbsp;&nbsp;&nbsp;**Cuidados Relacionados:**")
                     for cuidado in cuidados_relacionados.split('\t'):
-                        st.markdown(
-                            "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;• " + cuidado, unsafe_allow_html=True)
+                        st.markdown("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;• " + cuidado, unsafe_allow_html=True)
 
-        observacoes = st.text_area(
-            "Observações gerais sobre os diagnósticos selecionados:", "")
-        avaliacao = st.slider("Avalie a aplicação (de 1 a 5):", 1, 5)
 
-        if st.button("Salvar Avaliação"):
-            # Registro do tempo de fim da seleção de sintomas
-            fim_selecao = time.time()
-            tempo_decorrido = round(fim_selecao - inicio_selecao, 2)
-            salvar_arquivo(iniciais_usuario, diagnosticos_selecionados,
-                           sintomas, observacoes, avaliacao, tempo_decorrido)
+    observacoes = st.text_area("Observações gerais sobre os diagnósticos selecionados:", "")
+    avaliacao = st.slider("Avalie a aplicação (de 1 a 5):", 1, 5)
+
+    if st.button("Salvar Avaliação"):
+        # Registro do tempo de fim da seleção de sintomas
+        fim_secao = time.time()
+        tempo_decorrido = round(fim_secao - inicio_secao, 2)
+        salvar_arquivo(iniciais_usuario, diagnosticos_selecionados, sintomas, observacoes, avaliacao, tempo_decorrido)
 
 
 def salvar_arquivo(iniciais_usuario, diagnosticos_selecionados, sintomas, observacoes, avaliacao, tempo_decorrido):
@@ -114,14 +125,16 @@ def salvar_arquivo(iniciais_usuario, diagnosticos_selecionados, sintomas, observ
 
             if file.tell() == 0:
                 writer.writerow(['Iniciais do Usuário', 'Diagnóstico', 'Sintomas', 'Observações', 'Avaliação', 'Tempo de Seleção (s)'])
-                
+
             for diagnostico, sintomas_diagnostico in diagnosticos_selecionados.items():
-                writer.writerow([iniciais_usuario, diagnostico, ", ".join(
-                    sintomas_diagnostico), observacoes, avaliacao, tempo_decorrido])
+                sintomas_formatados = [formatar_diagnostico(sintoma) for sintoma in sintomas_diagnostico]
+                writer.writerow([iniciais_usuario, diagnostico, ", ".join(sintomas_formatados), observacoes, avaliacao, tempo_decorrido])
         st.success("Avaliação salva com sucesso.")
     except Exception as e:
         st.error(f"Ocorreu um erro ao salvar os dados: {str(e)}")
 
 
 if __name__ == "__main__":
+    main()
+
     main()
